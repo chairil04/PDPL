2a3a5c8aca7dbd1bea7710d8114399b4815e1b19
chairil10 <chairilhs10@gmail.com>
2020-05-13 16:09:21 +0700

    praktikum 9 proxy PDPL

---
+++ proxy/ProxyPatternExample.java
@@ -0,0 +1,8 @@
+public class ProxyPatternExample {
+	public static void main(String[] args) 
+	{
+		System.out.println("***01102167054 demo proxy patern***\n");
+		Proxy px = new Proxy();
+		px.doSomeWork();
+	}
+}
